2476a020c0238facfe2cd15b45184f6090e8a545
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2020-02-29 14:02:55 +0100

    not used view model

---
+++ app/src/main/java/eu/chessout/v2/ui/dashboard/MyViewModel.kt
@@ -0,0 +1,26 @@
+package eu.chessout.v2.ui.dashboard
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import eu.chessout.v2.R
+
+class MyViewModel() : ViewModel() {
+
+//    val dashboardModelList = MutableLiveData<List<DashboardModel>>().apply {
+//        value = getInitialList()
+//    }
+    val dashboardModelList: MutableLiveData<List<DashboardModel>> = MutableLiveData()
+//
+//    private fun getInitialList(): List<DashboardModel> {
+//        var items: ArrayList<DashboardModel> = ArrayList()
+//        items.add(DashboardModel(R.drawable.chess_king_v1, "King"))
+//        items.add(DashboardModel(R.drawable.chess_queen_v1, "Queen"))
+//        items.add(DashboardModel(R.drawable.chess_bishop_v1, "Bishop"))
+//        items.add(DashboardModel(R.drawable.chess_knight_v1, "Knight"))
+//        items.add(DashboardModel(R.drawable.chess_rook_v1, "Rook"))
+//        items.add(DashboardModel(R.drawable.chess_pawn_v1, "Pawn"))
+//        items.add(DashboardModel(R.drawable.chess_king_and_rook_v1, "King and rook"))
+//        return items
+//    }
+
+}
