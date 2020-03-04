ce1c635489416794765ff3e3fe9afd7feaed6a76
Bogdan <bogdan.oloeriu@gmail.com>
2020-03-05 00:29:56 +0100

    wired up the queues with postman

--- backend/src/main/java/eu/chessout/v2/controller/BasicApiController.java
+++ backend/src/main/java/eu/chessout/v2/controller/BasicApiController.java
@@ -1,5 +1,12 @@
 package eu.chessout.v2.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +19,24 @@ import eu.chessout.shared.model.MyPayLoad;
 public class BasicApiController {
 
     @PutMapping("/api/gameResultUpdated")
-    public String gameResultUpdated(@RequestBody MyPayLoad myPayLoad) {
+    public String gameResultUpdated(@RequestBody MyPayLoad myPayLoad) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String stringPlayLoad = objectMapper.writeValueAsString(myPayLoad);
+
+        Queue queue = QueueFactory.getDefaultQueue();
+        queue.add(TaskOptions.Builder.withUrl("/api/gameResultUpdatedTask").payload(stringPlayLoad));
+        System.out.println("Hello gameResultUpdated");
         return "Greetings from gameResultUpdated";
+    }
+
+    @PostMapping(
+            value = "/api/gameResultUpdatedTask"
+            //consumes = {MediaType.TEXT_PLAIN_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String gameResultUpdatedTask(@RequestBody String payLoadBody) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        MyPayLoad myPayLoad = objectMapper.readValue(payLoadBody, MyPayLoad.class);
+        System.out.println("Hello gameResultUpdatedTask");
+        return "Greetings from gameResultUpdatedTask";
     }
 }
