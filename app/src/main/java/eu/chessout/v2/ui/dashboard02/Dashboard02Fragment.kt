e1b9812b44eee7b993e67a2bd29f1d08dbd1142c
Bogdan <bogdan.oloeriu@gmail.com>
2020-03-08 20:25:17 +0100

    added functional 100 seconds count

--- app/src/main/java/eu/chessout/v2/ui/dashboard02/Dashboard02Fragment.kt
+++ app/src/main/java/eu/chessout/v2/ui/dashboard02/Dashboard02Fragment.kt
@@ -4,38 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import eu.chessout.v2.R
+import kotlinx.android.synthetic.main.fragment_dashboard02.*
 
 class Dashboard02Fragment : Fragment() {
 
-    companion object {
-        fun newInstance() = Dashboard02Fragment()
-    }
 
     private lateinit var viewModel: Dashboard02ViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProviders.of(this).get(Dashboard02ViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard02, container, false)
-        val textView: TextView = root.findViewById(R.id.text_detail_2)
-        viewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_dashboard02, container, false)
     }
-/*
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(Dashboard02ViewModel::class.java)
-        // TODO: Use the ViewModel
-    }*/
 
+        viewModel.text.observe(this, Observer {
+            text_detail_2.text = it
+        })
+
+        viewModel.countValue.observe(this, Observer {
+            icon_text_count.text = "Count value: $it"
+        })
+
+        viewModel.initializeCount()
+    }
 
 }
--- app/src/main/java/eu/chessout/v2/ui/dashboard02/Dashboard02ViewModel.kt
+++ app/src/main/java/eu/chessout/v2/ui/dashboard02/Dashboard02ViewModel.kt
@@ -3,10 +3,34 @@ package eu.chessout.v2.ui.dashboard02
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.concurrent.thread
 
 class Dashboard02ViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "THis is detail 2 fragment"
+        value = "This is detail 2 fragment"
     }
+
+    private val _showItem = MutableLiveData<Boolean>().apply {
+        value = true
+    }
+
+    private val _countValue = MutableLiveData<Int>().apply {
+        value = 0;
+    }
+
     val text: LiveData<String> = _text
+    val showItem: LiveData<Boolean> = _showItem
+    val countValue: LiveData<Int> = _countValue
+
+
+    fun initializeCount() {
+        thread() {
+            while (countValue.value!! < 100) {
+                _countValue.postValue(countValue.value!! + 1)
+                Thread.sleep(1000)
+            }
+
+        }
+    }
+
 }
