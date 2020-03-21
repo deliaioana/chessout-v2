105330184eeac854fc4460475aa707dadcb598ee
Bogdan <bogdan.oloeriu@gmail.com>
2020-03-21 21:24:48 +0100

    added generated interfaces

--- app/src/main/java/eu/chessout/v2/ui/club/joinclub/JoinClubAdapter.kt
+++ app/src/main/java/eu/chessout/v2/ui/club/joinclub/JoinClubAdapter.kt
@@ -1,12 +1,28 @@
 package eu.chessout.v2.ui.club.joinclub
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import eu.chessout.shared.model.Club
 import eu.chessout.v2.R
 
-class JoinClubAdapter(var clubList: ArrayList<Club>) {
+class JoinClubAdapter(
+    var clubList: ArrayList<Club>
+) : RecyclerView.Adapter<JoinClubAdapter.ItemHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
     class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textView = itemView.findViewById<TextView>(R.id.list_item_text_simple_view)
