147912efde0edb65039c9bb8e6d8fde5fc112024
Bogdan <bogdan.oloeriu@gmail.com>
2020-04-01 15:29:02 +0200

    red round arguments in round state fragment

--- app/src/main/java/eu/chessout/v2/ui/tournament/tournaments/rounds/state/RoundStateFragment.kt
+++ app/src/main/java/eu/chessout/v2/ui/tournament/tournaments/rounds/state/RoundStateFragment.kt
@@ -6,14 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import eu.chessout.v2.R
+import kotlinx.android.synthetic.main.round_state_fragment.*
 
-class RoundStateFragment(clubId: String, tournamentId: String, roundId: Int) : Fragment() {
+class RoundStateFragment(
+    private val clubId: String,
+    private val tournamentId: String,
+    private val roundId: Int
+) : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.round_state_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        textView.text = "Round number $roundId"
     }
 }
